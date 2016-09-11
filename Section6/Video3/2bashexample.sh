f66e8501373b68c4fdc6c4ce4c69fcf5c182345e
Dave Mangot <github@mangot.com>
2016-09-11 15:28:54 -0700

    change storage schema

--- Section6/Video3/2bashexample.sh
+++ Section6/Video3/2bashexample.sh
@@ -5,3 +5,8 @@ while [ 1 ]
    echo callcenter.volume.numcalls $number `date +%s` | nc -w 3 localhost 22003
    sleep 5
  done
+
+
+# [callcenter]
+# pattern = ^callcenter\.
+# retentions = 5s:1w, 60s:1y
