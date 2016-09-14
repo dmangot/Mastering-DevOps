399e1ae34b6bd5c1d68d104b74ee403112fc81ec
Dave Mangot <github@mangot.com>
2016-09-13 23:50:33 -0700

    enhance bot

--- Section7/Video3/deploy.py
+++ Section7/Video3/deploy.py
@@ -7,10 +7,15 @@ def deploy(app):
     encoded_app = app.encode()
     app_hash = hashlib.md5(encoded_app)
 
+    with open('/Users/dave/work/hostlist', 'r') as content_file:
+       content = content_file.read()
     # sendToGraphite(deploys.production,1)
+    # scp something to AWS
 
-    return "deploying app  `{}` with hash `{}` to `production`".format(app,
-            app_hash.hexdigest())
+    return "deploying app  `{}` with hash `{}` to `production` ip `{}`".format(app,
+            app_hash.hexdigest(), content.rstrip())
+
+
 
 def on_message(msg, server):
     text = msg.get("text", "")
